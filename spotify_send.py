ececd7863fa3e286c8db727629fdffdec92fded6
Yash Yadav <yashyadav.sr@gmail.com>
2024-04-08 10:49:46 +0530

    feat: init

---
+++ spotify_send.py
@@ -0,0 +1,82 @@
+import requests
+import os
+import dotenv
+from flask import Flask, request, redirect, render_template
+from urllib.parse import urlencode
+from status_discord import set_status
+
+global song_is_playing
+
+
+dotenv.load_dotenv()
+
+spotify_client_id = os.getenv("SPOTIFY_CLIENT_ID")
+spotify_client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+redirect_uri = os.getenv("REDIRECT_URI")
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    params = {
+        "client_id": spotify_client_id,
+        "response_type": "code",
+        "redirect_uri": redirect_uri,
+        "scope": "user-read-currently-playing",  # Add other scopes if needed
+    }
+    url = "https://accounts.spotify.com/authorize?" + urlencode(params)
+    return redirect(url)
+
+@app.route('/callback')
+def callback():
+    code = request.args.get('code')
+    data = {
+        "grant_type": "authorization_code",
+        "code": code,
+        "redirect_uri": redirect_uri,
+        "client_id": spotify_client_id,
+        "client_secret": spotify_client_secret,
+    }
+    response = requests.post("https://accounts.spotify.com/api/token", data=data)
+    response_data = response.json()
+    access_token = response_data['access_token']
+    # Save the access token to a file
+    with open("access_token.txt", "w") as f:
+        f.write(access_token)
+    return "Access token saved!"
+
+@app.route('/get_currently_playing')
+def get_currently_playing():
+    with open("access_token.txt") as f:
+        access_token = f.read()
+    headers = {
+        "Authorization": f"Bearer {access_token}"
+    }
+    response = requests.get("https://api.spotify.com/v1/me/player/currently-playing", headers=headers)
+    try:
+        song_is_playing = response.json()
+        return response.json()
+    except:
+        return None
+    
+@app.route('/set_status', methods=["POST"])
+def set_status_route():
+    try:
+        song = get_currently_playing()
+        if song is None:
+            return {"current_song": None, "error": "An error occurred"}
+        # set_status(song)
+        return song
+    except:
+        return {"error": "An error occurred"}
+
+@app.route('/user_interface')
+def user_interface():
+    global song_is_playing
+    song_is_playing=get_currently_playing()
+    if song_is_playing is None:
+        song_is_playing = {"current_song": None}
+    return render_template("index.html", song=song_is_playing)
+
+if __name__ == "__main__":
+    app.run(port=5000, debug=True)
---
+++ status_discord.py
@@ -0,0 +1,49 @@
+import requests
+from dotenv import load_dotenv
+import json
+import os
+
+import time  # Import the time module
+import logging  # Import the logging module
+
+load_dotenv()  # Load environment variables from .env file
+
+url = 'https://discord.com/api/v9/users/@me/settings'
+headers = {
+    'authorization': os.getenv('DISCORD_TOKEN'),
+    'content-type': 'application/json',
+    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/95.0.4638.69 Safari/537.36',
+    'accept': '*/*',
+}
+
+def get_current_song():
+    response = requests.get('http://localhost:5000/get_currently_playing')
+    if response.status_code != 200:
+        return None
+    return response.json()
+
+# Set up logging
+# Replace yashy with your username
+logging.basicConfig(filename='/Users\yashy\.custom-discord-status/discord_status.log', level=logging.INFO, format='%(asctime)s %(message)s')
+
+def set_status(song):
+    res = get_current_song()
+    if res is None:
+        i = '🎵 Not listening to anything'
+    elif 'item' in res:
+        song = res['item']['name']
+        artist = res['item']['artists'][0]['name']
+        i = f'Currently listening 🎵 {song} - {artist}'
+    else:
+        i = '🎵 Not listening to anything'
+    data = json.dumps({
+        "custom_status": {
+            "text": i
+        }
+    })
+    response = requests.patch(url, headers=headers, data=data)
+    print(response)
+    print('for I: ', i)
+    logging.info(f'Response Code: {response.status_code}')
+    logging.info(f'Status set to: {i}')
+    return response
